0962b7a08af65fe65f43a4164b0977c8a5ca2462
zeein <zeein81@gmail.com>
2022-11-04 21:58:35 +0900

    point fixed

--- slbs/assets/js/myshop/zMyshop.js
+++ slbs/assets/js/myshop/zMyshop.js
@@ -1,23 +1,38 @@
 (function () {
-  const href = window.location.href;
-  const lastUrl = href.match(/([^\/]*)\/*$/)[1];
-  let goIndex = 0;
-  if (href.includes('order/list.html')) {
-    goIndex = 1;
-  }
-  if (href.includes('myshop/wish_list.html')) {
-    goIndex = 2;
-  }
-
   function navbar(el) {
+    const href = window.location.href;
+    const lastUrl = href.match(/([^\/]*)\/*$/)[1];
+    let goIndex = 0;
+
+    if (href.includes('order/list.html')) {
+      goIndex = 1;
+    }
+    if (href.includes('myshop/wish_list.html')) {
+      goIndex = 2;
+    }
+    if (href.includes('/myshop/board_list.html')) {
+      goIndex = 3;
+    }
+    if (href.includes('coupon.html')) {
+      goIndex = 4;
+    }
+    if (href.includes('historyList.html')) {
+      goIndex = 5;
+    }
+    // console.log(href, lastUrl, goIndex);
     if (goIndex !== 0) {
       el.querySelector('.ul-nav-myshop .item:nth-child(' + Number(goIndex) + ')').classList.add(
         'is-active'
       );
     }
   }
-  const components = document.querySelectorAll('[data-component="navbar"]');
-  Array.from(components).forEach((el) => {
-    new navbar(el);
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const components = document.querySelectorAll('[data-component="navbar"]');
+    Array.from(components).forEach((el) => {
+      setTimeout(() => {
+        new navbar(el);
+      }, 50);
+    });
   });
 })();
